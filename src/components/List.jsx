3a34c524f9bedff5ebac94ffa37551c7c9992dbb
cristian prieto <cristianemanuelprieto@gmail.com>
2022-08-04 18:14:16 +0200

    today

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,17 +1,26 @@
+import { format } from "date-fns";
 import styles from "./List.module.css";
-// TODO: Install date-fns and format date: `npm install date-fns`
-// import { format } from "date-fns";
+import { BsStarFill, BsStar } from "react-icons/bs";
 
 export function List({ data }) {
   return (
-    <ul>
+    <ul className={styles.listBox}>
+      <div className={styles.listHeader}>
+        <span className={styles.title}>Title</span>
+        <span className={styles.rating}>Rating</span>
+        <span className={styles.comment}>Comment</span>
+        <span className={styles.date}>Added</span>
+      </div>
       {data.map((item) => (
         <li key={item.id} className={styles.item}>
-          <div>{item.title} </div>
-          <div>{item.rating} </div>
-          <div>{item.comment} </div>
-          {/* format(item.createdAt, "dd/MM/yyyy HH:mm") */}
-          <div> / created at: {item.createdAt.toString()}</div>
+          <div className={styles.title}>{item.title} </div>
+          <div className={styles.rating}>{item.rating}</div>
+          <div className={styles.comment}>{item.comment} </div>
+          <div className={styles.date}>
+            {format(item.createdAt, "dd/MM/yyyy")}
+          </div>
+
+          {/* <div> / created at: {item.createdAt.toString()}</div> */}
         </li>
       ))}
     </ul>
