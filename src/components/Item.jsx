fd18e22ed3cd4be6b2f3e9651d1d646b4f492863
Julián Prieto <herbie.juls@gmail.com>
2022-08-08 16:20:35 -0300

    Fix Item and add details

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,37 +1,71 @@
 import { format } from "date-fns";
+import { useState } from "react";
 import { BsStarFill, BsStar } from "react-icons/bs";
 import { getTypeColor } from "../utils/utils";
 import { Button } from "./Button";
 import styles from "./Item.module.css";
 
 export function Item({ item }) {
+  const [showMobileDetails, setShowMovileDetails] = useState(false);
+
   return (
-    <li key={item.id} className={styles.item}>
-      <div
-        style={{
-          borderLeft: `18px solid ${getTypeColor(item.type)}`,
-          width: 48,
-          minHeight: 45,
-          borderTopLeftRadius: "1rem",
-          borderBottomLeftRadius: "1rem",
-        }}
-        className={styles.title}
-      >
-        {item.title}
-      </div>
-      <div className={styles.rating}>
-        <div>
-          <BsStarFill />
+    <>
+      <li key={item.id} className={styles.item}>
+        <div
+          style={{
+            backgroundColor: getTypeColor(item.type),
+          }}
+          className={styles.type}
+        />
+        <div className={styles.title}>{item.title}</div>
+        <div className={styles.rating}>
+          {[0, 1, 2, 3, 4].map((rating) =>
+            item.rating > rating ? <BsStarFill key={rating} /> : <BsStar key={rating} />
+          )}
         </div>
-        <div>{item.rating >= 2 ? <BsStarFill /> : <BsStar />}</div>
-        <div>{item.rating >= 3 ? <BsStarFill /> : <BsStar />}</div>
-        <div>{item.rating >= 4 ? <BsStarFill /> : <BsStar />}</div>
-        <div>{item.rating === 5 ? <BsStarFill /> : <BsStar />} </div>
-      </div>
-      <div className={styles.comment}>{item.comment} </div>
-      <div className={styles.date}>{format(item.createdAt, "dd/MM/yyyy")}</div>
-      <Button classes={styles.delete}>x</Button>
-      {/* <div> / created at: {item.createdAt.toString()}</div> */}
-    </li>
+        <div className={styles.comment}>{item.comment}</div>
+        <div className={styles.added}>{format(item.createdAt, "dd/MM/yyyy")}</div>
+        <div className={styles.more}>
+          <Button classes={styles.moreButton} onClick={() => setShowMovileDetails((prev) => !prev)}>
+            {showMobileDetails ? "LESS" : "MORE"}
+          </Button>
+        </div>
+        <div className={styles.delete}>
+          <Button classes={styles.deleteButton}>x</Button>
+        </div>
+      </li>
+      {showMobileDetails && <div className={styles.item}>{item.comment}</div>}
+    </>
   );
 }
+// export function Item({ item }) {
+//   return (
+//     <li key={item.id} className={styles.item}>
+//       <div
+//         style={{
+//           borderLeft: `18px solid ${getTypeColor(item.type)}`,
+//           width: 48,
+//           minHeight: 45,
+//           borderTopLeftRadius: "1rem",
+//           borderBottomLeftRadius: "1rem",
+//         }}
+//         className={styles.title}
+//       >
+//         {item.title}
+//       </div>
+//       <div className={styles.rating}>
+//         <div>
+//           <BsStarFill />
+//         </div>
+//         <div>{item.rating >= 2 ? <BsStarFill /> : <BsStar />}</div>
+//         <div>{item.rating >= 3 ? <BsStarFill /> : <BsStar />}</div>
+//         <div>{item.rating >= 4 ? <BsStarFill /> : <BsStar />}</div>
+//         <div>{item.rating === 5 ? <BsStarFill /> : <BsStar />} </div>
+//       </div>
+//       <div className={styles.comment}>{item.comment} </div>
+//       <div className={styles.date}>{format(item.createdAt, "dd/MM/yyyy")}</div>
+//       <Button classes={styles.delete}>x</Button>
+//       {/* <div> / created at: {item.createdAt.toString()}</div> */}
+//     </li>
+//   );
+// }
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -4,12 +4,6 @@ import styles from "./List.module.css";
 export function List({ data }) {
   return (
     <ul className={styles.listBox}>
-      <div className={styles.listHeader}>
-        <span className={styles.title}>Title</span>
-        <span className={styles.rating}>Rating</span>
-        <span className={styles.comment}>Comment</span>
-        <span className={styles.date}>Added</span>
-      </div>
       {data.map((item) => (
         <Item key={item.id} item={item} />
       ))}
